37c6a211bcdc9cf9eb901c6974b78f56a03b548e
Ana Laura Andrade <143561088+nalaurandrade@users.noreply.github.com>
2024-04-17 21:25:46 -0300

    Add files via upload

---
+++ atividadepilha2/atividadepilha2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.Design;
+using System.Linq;
+
+Stack<int> pilha = new Stack<int>();
+
+pilha.Push(2);
+pilha.Push(4);
+pilha.Push(10);
+pilha.Push(20);
+pilha.Push(12);
+pilha.Push(6);
+
+var ultimoItem = pilha.Peek();
+
+for (int i = 0; i < 10; i++)
+{
+    if (pilha.ElementAt(i) % 2 == 0)
+    {
+    Console.WriteLine($"Linha {i} - [{pilha.ElementAt(i)}]");
+    }
+
+    if (pilha.ElementAt(i) % 3 == 0)
+    {
+        Console.WriteLine($"Linha {i} - [{pilha.ElementAt(i)}]");
+    }
+}
+
